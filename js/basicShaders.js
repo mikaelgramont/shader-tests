c1622a6894eb02983e7db3a6b7be1ea6dd30590b
Mikael Gramont <mgramont@gmail.com>
2012-09-19 21:50:09 -0700

    .

--- js/basicShaders.js
+++ js/basicShaders.js
@@ -1,4 +1,15 @@
 (function() {
+	var Parameters = function(){
+		this.dividor = 1.0;
+		this.refresh = function() {
+			update();
+		}
+	};
+	var parameters = new Parameters();
+	var gui = new dat.GUI();
+	gui.add(parameters, 'dividor', 1, 100);
+
+
 	var canvas = document.getElementById('maincanvas');
 	var renderer = new THREE.WebGLRenderer({
 		canvas: canvas
@@ -8,21 +19,32 @@
 	var camera = new THREE.PerspectiveCamera( 60, (canvas.clientWidth / canvas.clientHeight), 1, 10000);
 	camera.position.z = 1000;
 
-	var geometry = new THREE.PlaneGeometry( 500, 500, 20, 20 );
+	var geometry = new THREE.PlaneGeometry( 1000, 1000, 20, 20 );
 	var shaderUniforms = {
-		rotAngle: {type: 'f', value: 0.0}
+		dividor: {type: 'f', value: 0.0},
+		rotAngle: {type: 'f', value: 0.0},
+		shaderType: {type: 'i', value: 1}
 	};
+	var vertexShader = document.getElementById( 'vertexShader' ).textContent;
+	var fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+
 	var material = new THREE.ShaderMaterial({
 		uniforms: shaderUniforms,
-		vertexShader:   document.getElementById( 'vertexShader' ).textContent,
-		fragmentShader: document.getElementById( 'fragmentShader' ).textContent,
+		vertexShader: vertexShader,
+		fragmentShader: fragmentShader
 	});
 	var mesh = new THREE.Mesh(geometry, material);
 
 	scene.add(mesh);
 	update();
 	
+	document.getElementById('param').addEventListener('onkeypress', function(e){
+		param = e.currentTarget.value;
+		shaderUniforms.param.value = parseFloat(param);
+	});
+
 	function update() {
+		shaderUniforms.dividor.value = parameters.dividor;
 		shaderUniforms.rotAngle.value += 0.001;
 		if (shaderUniforms.rotAngle.value > 2 * Math.PI) {
 			shaderUniforms.rotAngle.value -= 2 * Math.PI;
